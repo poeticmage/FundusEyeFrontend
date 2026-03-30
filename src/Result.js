863a4d8ce9552469225c251d88bba19345c4de92
Priyadarshi Mukhopadhyay <158742737+poeticmage@users.noreply.github.com>
2026-03-30 17:10:10 +0530

    Result.js
    result added

--- src/Result.js
+++ src/Result.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import axios from "axios";
 
-
+//https://image-ewt313hia-priyadarshi-mukhopadhyays-projects.vercel.app/
+//https://image-2-qj7i.onrender.com/image
 function Result({inp,handleClose}){
-    const API="https://image-2-qj7i.onrender.com/image";
+    const API="https://image-ewt313hia-priyadarshi-mukhopadhyays-projects.vercel.app/";
     const [resp,setRes]=React.useState(null);
     React.useEffect(()=>{
           console.log("inp received in Result:", inp);
